5052d53790ef38c5423f3b4390018ec06dcf13a4
tbesser <tristan.besser58@gmail.com>
2024-10-02 22:15:17 -0400

    Added Account Recovery Page design

---
+++ src/components/LoginFrame.vue
@@ -0,0 +1,24 @@
+
+<template>
+    <div class="rounded-box">
+      <slot></slot>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'RoundedBox',
+  };
+  </script>
+  
+  <style scoped>
+  .rounded-box {
+    display: flex;
+    flex-direction: column;
+    padding: 20px;
+    width: 624px;
+    background: white;
+    border-radius: 15px;
+  }
+  </style>
+  
