7d8a99feb88ba07c6d7f272043a00a7ead5ca556
Jack <jheineki@kent.edu>
2024-10-18 01:01:50 -0400

    added filtering to display

--- src/views/Schedule.vue
+++ src/views/Schedule.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 
+import { faAlignJustify } from '@fortawesome/free-solid-svg-icons/faAlignJustify';
 import { ref } from 'vue';
 import { useRouter } from 'vue-router';
 
@@ -9,8 +10,9 @@ defineOptions({
     name: 'scheduler',
 });
 
-const options = ref(['Location 1', 'Location 2', 'Location 3']);
+const options = ref(['Location 1', 'Location 2', 'Location 3', 'Show All']);
 const shifts = ref([]);
+const selectedOption = ref('Show All');
 
 class Shift {
     constructor(start_time, day, location, duration){
@@ -22,20 +24,34 @@ class Shift {
 }
 
 // Add initial shift
-shifts.value.push(new Shift(7, 0, 'main', 4));
-shifts.value.push(new Shift(4, 3, 'main', 8));
-shifts.value.push(new Shift(20, 3, 'main', 8));
-shifts.value.push(new Shift(14, 6, 'main', 4));
+shifts.value.push(new Shift(7, 0, 'Location 1', 4));
+shifts.value.push(new Shift(4, 3, 'Location 2', 8));
+shifts.value.push(new Shift(20, 3, 'Location 1', 8));
+shifts.value.push(new Shift(14, 6, 'Location 3', 4));
 
 function getShiftStyle(shift) {
-    return {
-        position: 'absolute',
-        top: `${50 + 50 * shift.day + shift.day * 3}px`,
-        left: `${4 + 4 * shift.start_time}%`,
-        width: `${4 * shift.duration}%`,
-        height:'50px',
-        backgroundColor: 'blue',
-    };
+    // Use the selectedOption value and compare it with the shift location
+    if(shift.location === selectedOption.value || selectedOption.value === 'Show All'){
+        return {
+            position: 'absolute',
+            top: `${50 + 50 * shift.day + 2 + shift.day * 1.5}px`,
+            left: `${4 + 4 * shift.start_time}%`,
+            width: `${4 * shift.duration}%`,
+            height: '50px',
+            backgroundColor: 'blue',
+            color: 'black',
+            display: 'flex',                // Center content
+            justifyContent: 'center',       // Horizontally center
+            alignItems: 'center',           // Vertically center
+            borderRadius: '10px',           // Rounded corners
+        };
+    }
+    else{
+        return {
+            display:'none',
+        }
+    }
+    return {};
 }
 </script>
 
@@ -70,7 +86,33 @@ function getShiftStyle(shift) {
                 <div v-for="(shift, index) in shifts" :key="index" :style="getShiftStyle(shift)">
                     Shift: {{ shift.start_time }} - {{ shift.duration }} hours
                 </div>
-                <div id="schedule-day">Day</div>
+                <div id="schedule-day">
+                    <div> Day </div>
+                    <div> 7 AM </div>
+                    <div> 8 AM </div>
+                    <div> 9 AM </div>
+                    <div> 10 AM </div>
+                    <div> 11 AM </div>
+                    <div> 12 PM </div>
+                    <div> 1 PM</div>
+                    <div> 2 PM</div>
+                    <div> 3 PM</div>
+                    <div> 4 PM</div>
+                    <div> 5 PM</div>
+                    <div> 6 PM</div>
+                    <div> 7 PM</div>
+                    <div> 8 PM</div>
+                    <div> 9 PM</div>
+                    <div> 10 PM</div>
+                    <div> 11 PM</div>
+                    <div> 12 AM</div>
+                    <div> 1 AM</div>
+                    <div> 2 AM</div>
+                    <div> 3 AM</div>
+                    <div> 4 AM</div>
+                    <div> 5 AM</div>
+                    <div> 6 AM</div>                    
+                </div>
                 <div id="schedule-day">Mon</div>
                 <div id="schedule-day">Tue</div>
                 <div id="schedule-day">Wed</div>
@@ -97,22 +139,19 @@ function getShiftStyle(shift) {
         left: 16%;
         height:50px;
         width:50px;
-        background-color: aquamarine;
     }
     #schedule-container {
         display:flex;
         flex-direction: column;
         width: 100%;
         height:100%;
-        background-color: rgb(188, 141, 141);
         position:relative;
     }
     #schedule-day {
         display:flex;
         flex-direction: row;
         height:50px;
-        background-color: rgb(105, 56, 56);
-        border-bottom: 2px solid black;
+        border-bottom: 2px solid #A0A0A0;
         align-items: center;
         justify-content: space-between;
 
@@ -122,7 +161,8 @@ function getShiftStyle(shift) {
         flex-direction: row;
         height:100%;
         justify-content: space-between;
-        background-color: #b8b8b8;
+        background-color: white;
+        color: #2857A3;
     }
     
     #scheduler {
